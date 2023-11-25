57e0fdefa9bec65765b5341ceb9b96a0e33e3b4e
SilencedTick880 <78270298+SilencedPlayz@users.noreply.github.com>
2023-11-25 19:12:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,17 @@ function toggleMenu() {
 }
 
 function loadContent(page) {
-    var contentArea = document.querySelector('.content');
-    
-    // Customize the content based on the selected page
-    switch (page) {
-        case 'home':
-            contentArea.innerHTML = '<h2>Welcome to our website!</h2><p>This is the home page content.</p>';
-            break;
-        case 'about':
-            contentArea.innerHTML = '<h2>About Us</h2><p>Learn more about our company and mission.</p>';
-            break;
-        case 'services':
-            contentArea.innerHTML = '<h2>Our Services</h2><p>Explore the services we offer to our clients.</p>';
-            break;
-        case 'contact':
-            contentArea.innerHTML = '<h2>Contact Us</h2><p>Get in touch with our team.</p>';
-            break;
-        default:
-            contentArea.innerHTML = '<h2>Welcome to our website!</h2><p>This is the default content.</p>';
-    }
+    var contentContainer = document.getElementById('content-container');
+
+    // Load content from separate HTML files
+    fetch(`pages/${page}.html`)
+        .then(response => response.text())
+        .then(data => {
+            contentContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading content:', error);
+        });
 
     // Close the menu after selecting a page
     toggleMenu();
