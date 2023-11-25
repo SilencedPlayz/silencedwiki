ba4dca7f0a8f04f8f96d280ddc2fc48e789649b8
SilencedTick880 <78270298+SilencedPlayz@users.noreply.github.com>
2023-11-25 18:36:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSliderMenu() {
+    document.querySelector('.slider-menu').style.width = '250px';
+    document.querySelector('.content').style.marginLeft = '250px';
+}
+
+function closeSliderMenu() {
+    document.querySelector('.slider-menu').style.width = '0';
+    document.querySelector('.content').style.marginLeft = '0';
+}
